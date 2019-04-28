cd301057f3beaa9b132c647e13546bdabdf82110
fuyuning <fuyuning@zhumengyuan.com>
2019-04-28 14:42:39 +0800

    new

--- cache/old/old_HomePageBannerLibrary.py
+++
@@ -1,9 +0,0 @@
-from robot_car_wash_wxmp_library.common import CommonLibrary
-
-
-class HomePageBannerLibrary(CommonLibrary):
-    def get_home_page_banners(self):
-        url = "{SERVER_DOMAIN}/home_page_banners".format(
-            SERVER_DOMAIN=self.SERVER_DOMAIN)
-        return self.client.get(url)
-
